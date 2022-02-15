79d684afe4c9e5479836d79a130e467973e241aa
Oluwadamiloju Yusuf <yusufdamiloju@gmail.com>
2022-02-15 13:41:43 +0200

    testing software

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 /**
- * @brief Example usage of GPIO peripheral. Three LEDs are toggled using 
- * GPIO functionality. A hardware-to-software interrupt is set up and 
+ * @brief Example usage of GPIO peripheral. Three LEDs are toggled using
+ * GPIO functionality. A hardware-to-software interrupt is set up and
  * triggered by a button switch.
  *
  * The tsb0 board has three LEDs (red, green, blue) connected to ports
  * PB11, PB12, PA5 respectively. The button switch is connected to port
- * PF4. LED and button locations (pin and port numbers) can be found from 
+ * PF4. LED and button locations (pin and port numbers) can be found from
  * the tsb0 board wiring schematics.
  *
  * EFR32 Application Note on GPIO
@@ -14,12 +14,12 @@
  * EFR32MG12 Wireless Gecko Reference Manual (GPIO p1105)
  * https://www.silabs.com/documents/public/reference-manuals/efr32xg12-rm.pdf
  *
- * GPIO API documentation 
+ * GPIO API documentation
  * https://docs.silabs.com/mcu/latest/efr32mg12/group-GPIO
- * 
+ *
  * ARM RTOS API
  * https://arm-software.github.io/CMSIS_5/RTOS2/html/group__CMSIS__RTOS.html
- * 
+ *
  * Copyright Thinnect Inc. 2019
  * Copyright ProLab TTÜ 2022
  * @license MIT
@@ -40,6 +40,9 @@
 #include "loggers_ext.h"
 #include "logger_fwrite.h"
 
+#include "em_cmu.h"
+#include "em_gpio.h"
+
 #include "loglevels.h"
 #define __MODUUL__ "main"
 #define __LOG_LEVEL__ (LOG_LEVEL_main & BASE_LOG_LEVEL)
@@ -49,32 +52,50 @@
 #include "incbin.h"
 INCBIN(Header, "header.bin");
 
+// declare buzzer function
+void buzzer_loop();
+
 // Heartbeat thread, initialize GPIO and print heartbeat messages.
-void hp_loop ()
+void hp_loop()
 {
-    #define ESWGPIO_HB_DELAY 10 // Heartbeat message delay, seconds
-    
+#define ESWGPIO_HB_DELAY 10 // Heartbeat message delay, seconds
+
     // TODO Initialize GPIO.
-    
+    CMU_ClockEnable(cmuClock_GPIO, true);
+
+    // Set up buzzer pins
+    GPIO_PinModeSet(gpioPortA, 0, gpioModePushPull, 0);
+
+    // create a thread/task for buzzer
+    const osThreadAttr_t BUZZER_thread_attr = {.name = "BUZZER_thread_attr"};
+    osThreadNew(buzzer_loop, NULL, &BUZZER_thread_attr);
+
     for (;;)
     {
-        osDelay(ESWGPIO_HB_DELAY*osKernelGetTickFreq());
+        osDelay(ESWGPIO_HB_DELAY * osKernelGetTickFreq());
         info1("Heartbeat");
     }
 }
 
-// TODO LED toggle thread.
+// TODO buzzer thread.
+void buzzer_loop()
+{
+    for (;;)
+    {
+        GPIO_PinOutToggle(gpioPortA, 0);
+        osDelay(500);
+        info1("BUZZER_thread_attr");
+    }
+}
 
-// TODO Button interrupt thread.
-
-int logger_fwrite_boot (const char *ptr, int len)
+int logger_fwrite_boot(const char *ptr, int len)
 {
     fwrite(ptr, len, 1, stdout);
     fflush(stdout);
     return len;
 }
 
-int main ()
+int main()
 {
     PLATFORM_Init();
 
@@ -82,13 +103,13 @@ int main ()
     RETARGET_SerialInit();
     log_init(BASE_LOG_LEVEL, &logger_fwrite_boot, NULL);
 
-    info1("ESW-GPIO "VERSION_STR" (%d.%d.%d)", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH);
+    info1("ESW-GPIO " VERSION_STR " (%d.%d.%d)", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH);
 
     // Initialize OS kernel.
     osKernelInitialize();
 
     // Create a thread.
-    const osThreadAttr_t hp_thread_attr = { .name = "hp" };
+    const osThreadAttr_t hp_thread_attr = {.name = "hp"};
     osThreadNew(hp_loop, NULL, &hp_thread_attr);
 
     if (osKernelReady == osKernelGetState())
@@ -105,5 +126,6 @@ int main ()
         err1("!osKernelReady");
     }
 
-    for(;;);
+    for (;;)
+        ;
 }
